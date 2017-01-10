31327b38e1efe3b173febb2f5b426e335638991c
Takaten <takato.y7724@gmail.com>
2017-01-10 13:50:37 +0900

    reply the same msg bot

--- index.php
+++ index.php
@@ -5,4 +5,26 @@ require_once __DIR__ . '/vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
 
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+    error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+    error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+    error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+    error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
 
+foreach ($events as $event) {
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+        continue;
+    }
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+        continue;
+    }
+
+    $bot->replyText($event->getReplyToken(), $event->getText());
+}
