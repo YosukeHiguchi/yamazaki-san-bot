857030bb7c26278078903c9d9b456a16c8f68a98
Takaten <takato.y7724@gmail.com>
2017-01-16 13:06:59 +0900

    test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('config.php');
 require_once __DIR__ . '/vendor/autoload.php';
+writeDebugLog('Blah');
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
